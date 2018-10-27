388eaf70b34638cda503fcb5b7c398b80b824d96
matt961 <matt961@protonmail.ch>
2018-10-26 19:04:40 -0700

    init

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from redis import Redis, RedisError
+import os
+import socket
+
+# Connect to Redis
+redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    try:
+        visits = redis.incr("counter")
+    except RedisError:
+        visits = "<i>cannot connect to Redis, counter disabled</i>"
+
+    app.logger.info("%s is redis client", str(redis.client_getname()))
+    app.logger.info("%s is redis list", str(redis.client_list()))
+    html = "<h3>Hello {name}!</h3>" \
+            "<b>Hostname:</b> {hostname}<br/>" \
+            "<b>Visits:</b> {visits}"
+    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
+
